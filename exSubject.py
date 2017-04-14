104ba66a98153fcb2f67d536188341b471489451
yarbrough.b <yarbrough.b@husky.neu.edu>
2017-04-14 13:36:18 -0400

    prints subjects to single file

--- exSubject.py
+++ exSubject.py
@@ -1,12 +1,15 @@
 from __future__ import print_function 
 # Import the English cleanupper
 from nltk.corpus import stopwords
+
 import string
 # for file directory
 import os
 # Import the email modules we'll need
 from email.parser import Parser
 import email
+
+nf = open('subjects', 'w')
 
 translator = str.maketrans('', '', string.punctuation)
 for filename in os.listdir("."):
@@ -20,13 +23,14 @@ for filename in os.listdir("."):
         if subject != None:
             subject = subject.split(' ')
         
-            subject = [ word.translate(translator) for word in subject]
+            subject = [ word.translate(translator).lower() for word in subject]
             # filter out common words
             stops = stopwords.words('english')
-            filtered_subject = [word for word in subject if word not in stops ]
-            # write to new file
-            nf = open(filename + '.s', 'w')
-            nf.write(' '.join(filtered_subject))
+            filtered_subject = [word for word in subject if ( word not in stops and word != 'spam') ]
+            # write to file
+           # nf = open(filename + '.s', 'w')
+            nf.write(' '.join(filtered_subject) + '\r\n')
         else:
             nf = open(filename + '.s', 'w')
             nf.write('subjectless')
+nf.close()
