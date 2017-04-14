d398e1ee7445d3e388139c85ffab5b3820347d36
yarbrough.b <yarbrough.b@husky.neu.edu>
2017-04-14 00:23:12 -0400

    formats and saves subjects

--- exSubject.py
+++ exSubject.py
@@ -1,17 +1,32 @@
-# Import the email modules we'll need
-from email.parser import Parser
-
+from __future__ import print_function 
 # Import the English cleanupper
 from nltk.corpus import stopwords
+import string
+# for file directory
+import os
+# Import the email modules we'll need
+from email.parser import Parser
+import email
 
-# If the e-mail headers are in a file, uncomment these two lines:
-with open('tryme.eml') as fp:
-     headers = Parser().parse(fp)
+translator = str.maketrans('', '', string.punctuation)
+for filename in os.listdir("."):
+    if filename.endswith(".eml"):
+        # extract subject from each file in directory
+        fp = open(filename,"r", errors='ignore')
+        headers = Parser().parse(fp)
 
-#  Now the header items can be accessed as a dictionary:
-subject = headers['subject']
-
-# try to filter
-filtered_subject = [word for word in subject if word not in stopwords.words('english')]
-print(filtered_subject)
-
+        #  get the subject from the header
+        subject = headers['subject']
+        if subject != None:
+            subject = subject.split(' ')
+        
+            subject = [ word.translate(translator) for word in subject]
+            # filter out common words
+            stops = stopwords.words('english')
+            filtered_subject = [word for word in subject if word not in stops ]
+            # write to new file
+            nf = open(filename + '.s', 'w')
+            nf.write(' '.join(filtered_subject))
+        else:
+            nf = open(filename + '.s', 'w')
+            nf.write('subjectless')
