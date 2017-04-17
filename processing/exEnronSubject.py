d2cbd580bf866f7bb23f0f86d34c982d8bcf9074
byarbrough <yarbrough.b@husky.neu.edu>
2017-04-16 21:44:17 -0400

    crashing?

--- processing/exEnronSubject.py
+++ processing/exEnronSubject.py
@@ -17,14 +17,15 @@ for filename in os.listdir("."):
         # extract first line from each line in directory
         fp = open(filename,"r", errors='ignore')
         subject = fp.readline().lower()
-        subject = subject.replace('subjet: ','')
+        subject = subject.replace('subject: ','')
+
         if subject != '':
-            subject = [ word.translate(translator) for word in subject]
+            # subject = [ word.translate(translator) for word in subject]
             # filter out common words
             stops = stopwords.words('english')
             filtered_subject = [word for word in subject if ( word not in stops and word != 'spam') ]
             # write to file
-            nf.write(filename + ' ' + ' '.join(filtered_subject).lstrip().rstrip() + '\r\n')
+            nf.write(str(filtered_subject))
         else:
             # nf = open(filename + '.s', 'w')
             nf.write(filename + ' ' + 'subjectless')
