1027e8578cdc94cc10ce8ab919198ff4a4675548
JohnTerragnoli <streetmanguy1@gmail.com>
2017-04-19 12:47:06 -0400

    created 4 classifiers but unable to utilize them

--- Code and Text Files/matlab_classifiers/multipleClassifiers.m
+++ Code and Text Files/matlab_classifiers/multipleClassifiers.m
@@ -1,5 +1,84 @@
-%% Import Training Data and put in useable form
+%% Import Training/Test Data and Format for Classifiers
 clear
+
+%Import and Condense Training Data
 load('trainingDataAndLabels.mat');
 allWords_train = [words_spam;words_ham];
-labelsWithWords = [labels_train,allWords_train];
+
+%Make Training Labels into Strings
+nSpam = length(words_spam);
+nLabels = length(labels_train);
+labels_train_names = cell(nLabels,1);
+labels_train_names(1:nSpam) = {'Spam'};
+labels_train_names(nSpam+1:end) = {'Ham'};
+
+%Combine Labels and Data Training Data
+labelsWithWords_train = table(labels_train_names,allWords_train);
+
+
+%Import and Condense Testing Data
+load('testingDataAndLabels.mat');
+allWords_test = [words_spam_test;words_ham_test];
+
+% %Make Training Labels into Strings
+% nSpam = length(words_spam_test);
+% nLabels = length(labels_test);
+% labels_test_names = cell(nLabels,1);
+% labels_test_names(1:nSpam) = {'Spam'};
+% labels_test_names(nSpam+1:end) = {'Ham'};
+% 
+% %Combine Labels and Data Training Data
+% labelsWithWords_test = table(labels_test_names,allWords_test);
+
+
+
+%% Coarse KNN 
+%Generates Coarse KNN
+[coarseKNN_classifier, ~] = coarseKNN(labelsWithWords_train);
+
+%Predicts with Coarse KNN
+coarseKNN_predictions = coarseKNN_classifier.predictFcn(allWords_test);
+%I tried using the variable name 'allwords_train' but it still didn't like
+%that. I tried including the labels, since that's the exact format used
+%when creating this thing, but it didnt' like that eithe.  Any ideas? 
+
+%% Linear Discriminant 
+%Create linear Discriminant
+[linearDiscrim_classifier, ~] = linearDiscriminant(labelsWithWords_train);
+
+%% Linear SVM 
+%Create linear SVM
+[linearSVM_classifier, ~] = linearSVM(labelsWithWords_train);
+
+%% Logistic_Regression
+%Create Logistic_Regression
+[logisticRegression_classifier, ~] = logistic_regression(labelsWithWords_train);
+
+
+
+
+
+
+
+
+
+
+
+% load('testingDataAndLabels.mat');
+% allWords_test = [words_spam_test;words_ham_test];
+% 
+% %Make Labels into Strings
+% nSpam = length(words_spam_test);
+% nLabels = length(labels_test);
+% labels_test_names = cell(nLabels,1);
+% labels_test_names(1:nSpam) = {'Spam'};
+% labels_test_names(nSpam+1:end) = {'Ham'};
+% 
+% %Combine Labels and Data
+% labelsWithWords_test = table(labels_test_names,allWords_test);
+% 
+% % allWords_test = table(allWords_test);
+% linearDiscrim_predictions = linearDiscriminant(labelsWithWords_test);
+
+
+
