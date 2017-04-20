6d472b605b1cad22be50925c4d350c6e87be8cd4
yarbrough.b <yarbrough.b@husky.neu.edu>
2017-04-20 13:01:45 -0400

    file for tranforming labels into confusion matrix

---
+++ Code and Text Files/matlab_classifiers/labelsConfusion.m
@@ -0,0 +1,48 @@
+%% format labels into confusion matrix
+%   Formats data and plots into a confusion matrix
+%
+%   INPUT: true and predicted labels in nSamples x 1 matrix
+%       title and axis inputs are optional
+%
+%   Example:
+%   labelsConfusion(true_labels, p_labels, 'Emails', {'spam' 'ham' ''});
+%   
+%   Brian Yarbrough
+% 
+function labelsConfusion( true_labels, p_labels, title, axis )
+
+% check optional input argument
+switch nargin
+    case 1
+        error('Too few input arguments');
+    case 2
+        title = '';
+        axis = {unique(true_labels) ''};        
+    case 3
+        axis = {unique(true_labels) ''};        
+end
+
+% get sizes
+nClass = numel(unique(true_labels));
+nSamp = size(true_labels, 1);
+% init arrays
+truth = zeros(nClass, nSamp);
+predicted = zeros(nClass, nSamp); 
+
+% format truth
+for i = 1:nSamp
+    index = int32(true_labels(i)) + 1;
+    truth(index, i) = 1;
+end
+% format predictions
+for i = 1:size(p_labels,1)
+    index = int32(p_labels(i)) + 1;
+    predicted(index, i) = 1;
+end
+
+figure
+plotconfusion(truth, predicted, title);
+set(gca,'yticklabel', axis)
+set(gca,'xticklabel', axis)
+
+end
